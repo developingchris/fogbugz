e69b534f3c30ad0c3e1636779617d24dbf07815a
Chris Chandler <chris@developingchris.com>
2014-01-25 12:27:56 -0500

    started project object map

--- lib/fogbugz/api.rb
+++ lib/fogbugz/api.rb
@@ -1,22 +1,59 @@
 require 'open-uri'
 require 'ox'
+require 'fogbugz/project'
 
 module Fogbugz
-  # Your code goes here...
-  #
   class Api
     def initialize url, key
       self.url = url
       self.key = key
     end
-    attr_accessor :key, :url
+    attr_accessor :key, :url, :token
+
+    def full_url
+      if url =~ /^https/
+        url
+      else
+        "https://#{url}.fogbugz.com"
+      end
+    end
 
     def supported_version?
-      result = open("https://#{url}.fogbugz.com/api.xml") do |f|
+      result = get_xml "#{full_url}/api.xml"
+      (result.minversion.text.to_i..result.version.text.to_i).include? API_MIN
+    end
+
+    def auth username, password
+      resp = get_xml "#{full_url}/api.asp?cmd=logon&email=#{username}&password=#{password}"
+
+      self.token = resp.token.nodes[0].value
+      !token.nil?
+    end
+
+    def source_checkin
+
+    end
+
+    def projects
+      resp = get_xml "#{full_url}/api.asp?token=#{token}&cmd=listProjects"
+      projects = []
+      resp.projects.nodes do |project|
+        projects << Project.build(project)
+      end
+
+      projects
+    end
+
+    private
+    def get_xml request_url, debug=false
+      puts request_url if debug
+      result = open(request_url) do |f|
         f.read
       end
-      version = Ox.parse(result)
-      (version.response.minversion.text.to_i..version.response.version.text.to_i).include? API_MIN
+
+      puts result if debug
+      doc = Ox.parse(result)
+      doc.response
     end
   end
 end
--- lib/fogbugz/project.rb
+++ lib/fogbugz/project.rb
@@ -1,4 +1,10 @@
 module Fogbugz
   class Project
+    attr_accessor :name
+    def self.build node
+      proj = Project.new
+      proj.name = node.sProject
+      proj
+    end
   end
 end
